61c0890dd12f36ced0b78471df8035d8b18b9726
Caden <cadenannison@gmail.com>
2026-08-05 16:46:13 -0600

    Kick Push

--- infra/migrations/env.py
+++ infra/migrations/env.py
@@ -18,6 +18,7 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+
 def run_migrations_online() -> None:
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
